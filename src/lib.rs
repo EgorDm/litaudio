81a7ee684dd1ed67d88b63c82eb6e2b26c45ccf7
Egor Dmitriev <egordmitriev2@gmail.com>
2019-07-04 14:55:39 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod container;
+pub mod format;
+pub mod slice;
+pub mod storage;
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn it_works() {
+		assert_eq!(2 + 2, 4);
+	}
+}
---
+++ src/storage/mod.rs
@@ -0,0 +1,9 @@
+pub mod constructor;
+pub mod mutable;
+pub mod storage;
+pub mod impls;
+
+pub use constructor::*;
+pub use mutable::*;
+pub use storage::*;
+pub use impls::*;
