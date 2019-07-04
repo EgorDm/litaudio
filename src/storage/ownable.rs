d58af5f177f64d5530279abbcac917406ec2790b
Egor Dmitriev <egordmitriev2@gmail.com>
2019-07-04 21:31:57 +0200

    Added ownable and size trait variants for audio

---
+++ src/storage/ownable.rs
@@ -0,0 +1,15 @@
+use crate::format::*;
+use crate::storage::AudioStorageMut;
+use litcontainers::Ownable;
+
+/// Type can be turned or cloned into a container which owns its data.
+pub trait OwnableAudio<T: Sample, C: Dim, L: Dim, P: SamplePackingType>: Ownable<T, C, L> {
+	type OwnedAudioType: AudioStorageMut<T, C, L, P>;
+
+	/// Converts itself to a container which owns its data. No guarantees that it wont be the same
+	/// container if it is already owns its data.
+	fn owned_audio(self) -> Self::OwnedAudioType;
+
+	/// Clones it's data into a container which owns its data.
+	fn clone_owned_audio(&self) -> Self::OwnedAudioType;
+}
